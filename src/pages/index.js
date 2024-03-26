84700c8c37d9890fc33a8aaf9a8ae3d9f6ad3989
Mike <veldsinkie.mv@gmail.com>
2024-03-26 03:10:02 -0400

    minor style tweaks

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import { useEffect } from 'react';
 import useBaseUrl from '@docusaurus/useBaseUrl';
 
 export default function Home() {
-  const url = useBaseUrl('/docs/api/@reference');
+  const url = useBaseUrl('/docs/');
   useEffect(() => {
     window.location.href = url
   }, []);
