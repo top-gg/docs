00467c67d388ce23796ed1546d6de2ad2b203791
null8626 <vierofernando9@gmail.com>
2024-03-27 16:27:27 +0700

    fix: fix syntax highlighting

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -91,6 +91,7 @@ const config = {
       prism: {
         theme: prismThemes.github,
         darkTheme: prismThemes.dracula,
+        additionalLanguages: ['csharp', 'java', 'javascript', 'php', 'ruby']
       },
     }),
 };
