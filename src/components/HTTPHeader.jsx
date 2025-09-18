bc8dc251ca85c266ba2cb38522ff26c4fb38b5b2
null8626 <vierofernando9@gmail.com>
2025-09-18 15:35:39 +0700

    refactor: use CSS variables for HTTPHeader

--- src/components/HTTPHeader.jsx
+++ src/components/HTTPHeader.jsx
@@ -3,37 +3,9 @@ import styled from "styled-components";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 import { useColorMode } from "@docusaurus/theme-common";
 
-const darkModeMappings = {
-  POST: {
-    color: "#78b993",
-    background: "#0e1712",
-    border: "#193123",
-  },
-  GET: {
-    color: "#7979d4",
-    background: "#212133",
-    border: "#47476b",
-  },
-};
-
-const lightModeMappings = {
-  POST: {
-    color: "#317854",
-    background: "#e4f5ea",
-    border: "#c1e3d0",
-  },
-  GET: {
-    color: "#4a4aa5",
-    background: "#e8e8fb",
-    border: "#c9c9ee",
-  },
-};
-
-const getStyle = (method, colorMode) => (colorMode === "dark" ? darkModeMappings : lightModeMappings)[method];
-
 const HeaderWrapper = styled.div`
-  border: 1px solid ${(props) => getStyle(props.method, props.mode).border};
-  background: ${(props) => getStyle(props.method, props.mode).background};
+  border: 1px solid var(--http-header-border);
+  background: var(--http-header-background);
   border-radius: 4px;
   display: flex;
   align-items: center;
@@ -44,14 +16,14 @@ const HeaderWrapper = styled.div`
 `;
 
 const MethodName = styled.h2`
-  color: ${(props) => getStyle(props.method, props.mode).color};
+  color: var(--http-header-color);
   font-weight: 700;
   font-size: 14px;
   margin: 0 10px 0 0;
 `;
 
 const CopyButton = styled.button`
-  color: ${(props) => getStyle(props.method, props.mode).color};
+  color: var(--http-header-color);
   background: inherit;
   font-size: 14px;
   border: none;
@@ -88,7 +60,7 @@ export default function HTTPHeader({ type, path }) {
   const fullUrl = BASE_URL + path
   
   return (
-    <HeaderWrapper method={type} mode={colorMode}>
+    <HeaderWrapper method={type} className={type} mode={colorMode}>
       <Header>
         <MethodName method={type} mode={colorMode}>{type}</MethodName>
         <EndpointUrl
