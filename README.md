d8393a8ab1b21239b2800796a1a917706314cddb
dev-bun <66565286+dev-bun@users.noreply.github.com>
2025-11-24 13:53:39 +0000

    full redesign

--- README.md
+++ README.md
@@ -1,41 +1,50 @@
-# Website
+# Protocol
 
-This website is built using [Docusaurus](https://docusaurus.io/), a modern static website generator.
+Protocol is a [Tailwind UI](https://tailwindui.com) site template built using [Tailwind CSS](https://tailwindcss.com) and [Next.js](https://nextjs.org).
 
-### Installation
+## Getting started
 
-```
-$ yarn
-```
+To get started with this template, first install the npm dependencies:
 
-### Local Development
-
-```
-$ yarn start
+```bash
+npm install
+cp .env.example .env.local
 ```
 
-This command starts a local development server and opens up a browser window. Most changes are reflected live without having to restart the server.
+Next, run the development server:
 
-### Build
-
-```
-$ yarn build
+```bash
+npm run dev
 ```
 
-This command generates static content into the `build` directory and can be served using any static contents hosting service.
+Finally, open [http://localhost:3000](http://localhost:3000) in your browser to view the website.
 
-### Deployment
+## Customizing
 
-Using SSH:
+You can start editing this template by modifying the files in the `/src` folder. The site will auto-update as you edit these files.
 
-```
-$ USE_SSH=true yarn deploy
-```
+## Global search
 
-Not using SSH:
+By default this template uses [Algolia DocSearch](https://docsearch.algolia.com) for the global search. DocSearch is free for open-source projects, and you can sign up for an account on their website. Once your DocSearch account is ready, update the following [environment variables](https://nextjs.org/docs/basic-features/environment-variables) in your project with the values provided by Algolia:
 
 ```
-$ GIT_USER=<Your GitHub username> yarn deploy
+NEXT_PUBLIC_DOCSEARCH_APP_ID=
+NEXT_PUBLIC_DOCSEARCH_API_KEY=
+NEXT_PUBLIC_DOCSEARCH_INDEX_NAME=
 ```
 
-If you are using GitHub pages for hosting, this command is a convenient way to build the website and push to the `gh-pages` branch.
+## License
+
+This site template is a commercial product and is licensed under the [Tailwind UI license](https://tailwindui.com/license).
+
+## Learn more
+
+To learn more about the technologies used in this site template, see the following resources:
+
+- [Tailwind CSS](https://tailwindcss.com/docs) - the official Tailwind CSS documentation
+- [Next.js](https://nextjs.org/docs) - the official Next.js documentation
+- [Headless UI](https://headlessui.dev) - the official Headless UI documentation
+- [Framer Motion](https://www.framer.com/docs/) - the official Framer Motion documentation
+- [MDX](https://mdxjs.com/) - the official MDX documentation
+- [Algolia Autocomplete](https://www.algolia.com/doc/ui-libraries/autocomplete/introduction/what-is-autocomplete/) - the official Algolia Autocomplete documentation
+- [Zustand](https://docs.pmnd.rs/zustand/getting-started/introduction) - the official Zustand documentation
---
+++ src/components/Header.jsx
@@ -0,0 +1,104 @@
+import { forwardRef } from 'react'
+import Link from 'next/link'
+import clsx from 'clsx'
+import { motion, useScroll, useTransform } from 'framer-motion'
+import { useRouter } from 'next/router'
+import { Logo } from '@/components/Logo'
+import {
+  MobileNavigation,
+  useIsInsideMobileNavigation,
+} from '@/components/MobileNavigation'
+import { useMobileNavigationStore } from '@/components/MobileNavigation'
+import { ModeToggle } from '@/components/ModeToggle'
+import { getNavItems } from '@/components/Navigation'
+import {
+  MagnifyingGlassIcon,
+} from '@heroicons/react/24/outline'
+
+function TopLevelNavItem({ href, children }) {
+  return (
+    <li>
+      <Link
+        href={href}
+        className="text-sm leading-5 text-zinc-600 transition hover:text-zinc-900 dark:text-zinc-400 dark:hover:text-white"
+      >
+        {children}
+      </Link>
+    </li>
+  )
+}
+
+export const Header = forwardRef(function Header({ className }, ref) {
+  let router = useRouter()
+  let { isOpen: mobileNavIsOpen } = useMobileNavigationStore()
+  let isInsideMobileNavigation = useIsInsideMobileNavigation()
+
+  let { scrollY } = useScroll()
+  let bgOpacityLight = useTransform(scrollY, [0, 72], [0.5, 0.9])
+  let bgOpacityDark = useTransform(scrollY, [0, 72], [0.2, 0.8])
+
+  const navItems = getNavItems()
+
+  return (
+    <motion.div
+      ref={ref}
+      className={clsx(
+        className,
+        'fixed inset-x-0 top-0 z-50 flex h-20 items-center justify-between gap-12 px-4 transition sm:px-6 lg:left-72 lg:z-30 lg:px-8 xl:left-80',
+        !isInsideMobileNavigation &&
+          'backdrop-blur-sm dark:backdrop-blur lg:left-72 xl:left-80',
+        isInsideMobileNavigation
+          ? 'bg-white dark:bg-zinc-900'
+          : 'bg-white/[var(--bg-opacity-light)] dark:bg-zinc-900/[var(--bg-opacity-dark)]'
+      )}
+      style={{
+        '--bg-opacity-light': bgOpacityLight,
+        '--bg-opacity-dark': bgOpacityDark,
+      }}
+    >
+      <div
+        className={clsx(
+          'absolute inset-x-0 top-full h-px transition',
+          (isInsideMobileNavigation || !mobileNavIsOpen) &&
+            'bg-zinc-900/7.5 dark:bg-white/7.5'
+        )}
+      />
+      <div className="flex items-center gap-5 lg:hidden justify-right">
+        <Link href="/" aria-label="Home">
+          <Logo className="h-6" />
+        </Link>
+      </div>
+      <div className="flex gap-5">
+        <nav className="block">
+          <ul role="list" className="flex items-center gap-8 h-full hidden lg:flex">
+            <TopLevelNavItem href="https://top.gg">Website</TopLevelNavItem>
+            <TopLevelNavItem href="https://discord.gg/dbl">Support</TopLevelNavItem>
+          </ul>
+        </nav>
+        <div className="hidden md:block md:w-px md:bg-zinc-900/10 md:dark:bg-white/15 items-center" />
+        <div className="flex gap-4 justify-right">
+          <ModeToggle />
+          <select
+            className="flex items-center px-4 py-1 justify-center rounded-md text-black dark:text-white transition dark:bg-zinc-500/25 hover:bg-zinc-300/25 dark:hover:bg-white/25 block lg:hidden"
+            aria-label="Toggle dark mode"
+            onChange={(e) => {
+              router.push(e.target.value)
+              e.target.value = 'Select a location'
+            }}
+          >
+            <option selected hidden key="default">Select a location</option>
+            {navItems.map((item, index) => (
+              <optgroup label={item.title} key={index}>
+                {item.links.map((subItem, subIndex) => (
+                  <option value={subItem.href} key={subIndex}>
+                    {subItem.title}
+                  </option>
+                ))}
+              </optgroup>
+            ))}
+          </select>
+        </div>
+      </div>
+    </motion.div>
+  )
+})
